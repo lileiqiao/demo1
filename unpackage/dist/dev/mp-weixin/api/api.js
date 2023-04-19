adaa069ee8c9ee3df149a2f81a2f56cdcc2851d6
make <zqx15670303217@163.com>
2023-04-19 17:50:47 +0800

    删除未知文件夹

--- unpackage/dist/dev/mp-weixin/api/api.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const api_axios = require("./axios.js");
-const getTypeOne = () => api_axios.get("/home");
-const lbt = () => api_axios.get("/");
-exports.getTypeOne = getTypeOne;
-exports.lbt = lbt;
