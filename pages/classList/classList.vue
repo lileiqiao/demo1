4cd620ef10cb05815d49c3ec8eaf45d839319228
make <zqx15670303217@163.com>
2023-04-19 17:40:09 +0800

    进度1—1

---
+++ pages/classList/classList.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="box">111</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { onLoad } from '@dcloudio/uni-app';
+import { getTypeOne } from '@/api/api.js';
+let one = ref(0);
+let two = ref(0);
+let getdata = async () => {
+	let res = await getTypeOne();
+	console.log(res.listData);
+};
+getdata();
+onLoad(Option => {
+	console.log(Option);
+	uni.setNavigationBarTitle({
+		title: '新的标题'
+	});
+});
+</script>
+
+<style lang="scss" scoped>
+@import './classList.scss';
+</style>
--- pages/fenlei/fenlei.vue
+++ pages/fenlei/fenlei.vue
@@ -1,11 +1,86 @@
 <template>
-	<view class="">
-		2222
+	<view class="box">
+		<view class="search-input-container"><view class="search-input"></view></view>
+		<view class="content">
+			<view class="left">
+				<scroll-view scroll-y class="left_scroll">
+					<view
+						v-for="(it, i) in list"
+						:key="i"
+						:class="[i == active && 'active', ' left_item']"
+						@click="activecli(it.categoryId, i)"
+					>
+						{{ it.categoryName }}
+					</view>
+				</scroll-view>
+			</view>
+			<view class="right">
+				<scroll-view scroll-y="true" class="right_scroll" v-if="list.length">
+					<view class="right_tit">{{ list[0].categoryName }}</view>
+					<view v-for="(it, i) in contlist" class="right_item">
+						<view class="right_item_tit" @click="tolist(i, 0)">
+							{{ it.categoryName }}
+							<text style="float: right; margin-right: 30rpx;">></text>
+						</view>
+						<view class="right_item_cont">
+							<view
+								class="right_item_con_it"
+								v-for="(con, n) in it.subCategoryList"
+								@click="tolist(i, n)"
+							>
+								<image
+									:src="con.imageUrl"
+									mode=""
+									style="width: 134rpx; height: 134rpx;"
+								/>
+								<view>{{ con.categoryName }}</view>
+							</view>
+						</view>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+import { getTypeOne } from '@/api/api.js';
+export default {
+	data() {
+		return {
+			val: '',
+			list: [],
+			contlist: [],
+			active: 0,
+			id: 0
+		};
+	},
+	onLoad() {
+		this.getdata();
+	},
+	methods: {
+		async getdata() {
+			let res = await getTypeOne();
+			console.log(res.listData[0].subCategoryList);
+			this.list = res.listData;
+			this.contlist = res.listData[0].subCategoryList;
+			console.log();
+		},
+		activecli(act, i) {
+			this.id = act;
+			this.active = i;
+			this.contlist = this.list[i].subCategoryList;
+		},
+		tolist(i, n) {
+			console.log(i, n);
+			uni.navigateTo({
+				url:`/pages/classList/classList?one=${i}&two=${n}`
+			})
+		}
+	}
+};
 </script>
 
-<style>
+<style scoped lang="scss">
+@import './fenlei.scss';
 </style>
